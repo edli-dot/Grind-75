74846e16ec7f6caa1627e4be172f78083664ffb9
Edward Li <edwardli744@gmail.com>
2023-12-12 23:55:24 -0800

    Day 4

---
+++ ContainsDuplicates/ContainsDuplicates.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const cache = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!cache[nums[i]]) cache[nums[i]] = 1;
+    else return true;
+  }
+  return false;
+};
+
+//Time complexity of o(n)
+
+// Time complexity: O(n)
+// Space complexity: O(n)
+var containsDuplicate = function (nums) {
+  const s = new Set(nums);
+  return s.size !== nums.length;
+};
+
+//Can also create a set and comapre the size of teh Set compared to the length of the orignal array
