8d6cb4bc9cad4c63b19e4252b303566b5c7cd464
Edward Li <edwardli744@gmail.com>
2023-12-20 21:13:40 -0800

    two ways for mergetwolinkedlist

--- MergeTwoLinkedList/MergeTwoLinkedList.js
+++ MergeTwoLinkedList/MergeTwoLinkedList.js
@@ -12,22 +12,40 @@
  */
 var mergeTwoLists = function (list1, list2) {
   if (!list1 && !list2) return list1;
-  if (!list1) return list2;
-  if (!list2) return list1;
-
-  //need to compare position
-  list3 = new ListNode();
-  while (list1 && list2) {
-    let current = list1;
-    let current2 = list2;
-    if (list1.val <= list2.val || !list2.val) {
-      list3.next = list1.val;
-      current = list1.next;
-    } else if (list2.val <= list1.val || !list1.val) {
-      list3.next = list2.val;
-      current2 = list2.next;
-    }
-    return list3;
+  else if (!list1) return list2;
+  else if (!list2) return list1;
+  else if (list1.val <= list2.val) {
+    list1.next = mergeTwoLists(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoLists(list1, list2.next);
+    return list2;
   }
-  return list3;
 };
+// Recursively solves
+
+var mergeTwoLists = function (list1, list2) {
+  const dummy = new ListNode(0);
+  let current = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  // Attach the remaining elements (if any)
+  if (list1 !== null) {
+    current.next = list1;
+  } else {
+    current.next = list2;
+  }
+
+  return dummy.next;
+};
+//Solves iteratively
