412396f4e82b46998f0fa7886e82bf3be4ec8254
Edward Li <edwardli744@gmail.com>
2024-01-23 13:36:28 -0800

    log n time. implement a search for the middle value

--- 704_BinarySeearch/704_BinarySearch.js
+++ 704_BinarySeearch/704_BinarySearch.js
@@ -9,3 +9,29 @@ var search = function (nums, target) {
   }
   return -1;
 };
+//this is linear time. because we know it is a sorted array, we can make this faster
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let lowerB = 0,
+    upperB = nums.length - 1;
+  while (lowerB < upperB) {
+    let middle = lowerB + Math.floor((upperB - lowerB + 1) / 2);
+    if (nums[middle] > target) {
+      upperB = middle - 1;
+    } else {
+      lowerB = middle;
+    }
+  }
+  return nums[upperB] == target ? upperB : -1;
+};
+
+/**
+ * log n run time
+ * constantly check the middle number then change the reference
+ * how to implement this way because one test case
+ */
