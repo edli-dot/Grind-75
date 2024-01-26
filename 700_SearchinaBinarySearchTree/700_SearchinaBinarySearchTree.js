816af9cdf01aac8d6590859b292234e85110632f
Edward Li <edwardli744@gmail.com>
2024-01-26 11:43:29 -0800

    base cases first. pure recursion

--- 700_SearchinaBinarySearchTree/700_SearchinaBinarySearchTree.js
+++ 700_SearchinaBinarySearchTree/700_SearchinaBinarySearchTree.js
@@ -16,5 +16,4 @@ var searchBST = function (root, val) {
   if (root.val === val) return root;
   if (val < root.val) return searchBST(root.left, val);
   else if (val > root.val) return searchBST(root.right, val);
-  return null;
 };
