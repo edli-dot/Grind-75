338ee5752b8d880ad8b5be4407b4ed81258190c8
Edward Li <edwardli744@gmail.com>
2024-02-01 19:23:13 -0800

    porblem

---
+++ Find the missing digit x from the given expression/FindMissingDigitFromGivenExpression.js
@@ -0,0 +1,12 @@
+/**
+ * Input: S = “3x + 12 = 46”
+Output: 4
+Explanation:
+If we subtract 12 from 46, we will get 34.
+So, on comparing 3x and 34. the value of x = 4
+
+Input: S = “4 – 2 = x”
+Output: 2
+Explanation:
+After solving the equation, the value of x = 2.
+ */
