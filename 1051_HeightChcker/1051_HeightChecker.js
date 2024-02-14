9f26ad4a4c0d9145dd35e2e8d520a56a1c6280a3
Edward Li <edwardli744@gmail.com>
2024-02-14 00:49:41 -0800

    additional method to solve

--- 1051_HeightChcker/1051_HeightChecker.js
+++ 1051_HeightChcker/1051_HeightChecker.js
@@ -3,6 +3,22 @@
  * @return {number}
  */
 var heightChecker = function (heights) {
+  let answer = 0;
+  let sorted = [...heights].sort((a, b) => a - b);
+  for (let i = 0; i < heights.length; i++) {
+    if (sorted[i] !== heights[i]) answer++;
+  }
+  return answer;
+};
+
+//Initial thought process was to make a sorted array and then just compare the two
+// have to use [...] so that it makes a copy of teh array
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function (heights) {
   let sorted = [...heights].sort((a, b) => a - b);
   return heights.reduce(
     (total, height, index) =>
@@ -10,3 +26,6 @@ var heightChecker = function (heights) {
     0
   );
 };
+
+//Interesting usage in reduce
+//pass in height to compare the current element height in the array
