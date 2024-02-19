fe888fe744e4785fe27956916c22fc400f7ff5c1
Edward Li <edwardli744@gmail.com>
2024-02-18 18:09:49 -0800

    additionalk comments

--- 268_MissingNumber/268_MissingNumber.js
+++ 268_MissingNumber/268_MissingNumber.js
@@ -9,3 +9,10 @@ var missingNumber = function (nums) {
   }
   return Math.abs(answer);
 };
+
+
+/**
+ * Had the question stated that the the numbers dont necessarily start from 0
+ * i would have two more variables, one to track the lowest number and one tot rack the highest number
+ * perform a gaussian summatiohn to find the expected total and subtrtact that from the actual total
+ * then retunr the absolute value of the difference
