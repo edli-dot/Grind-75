ee2948b59614b8d1d57a347798ee9740965feaaf
Edward Li <edwardli744@gmail.com>
2024-02-23 18:52:12 +1100

    array

---
+++ 2656_MaximumSum/2656_MaximumSum.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maximizeSum = function (nums, k) {
+  let max = Math.max(...nums);
+  let sum = 0;
+  for (let i = 0; i < k; i++) {
+    sum += max;
+    max += 1;
+  }
+  return sum;
+};
