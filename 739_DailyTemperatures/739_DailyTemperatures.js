4da151863dac588de2bb209819ef522012334505
Edward Li <edwardli744@gmail.com>
2024-04-02 23:41:38 +1100

    monotonic stack

---
+++ 739_DailyTemperatures/739_DailyTemperatures.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  let answer = new Array(temperatures.length).fill(0);
+  let stack = [];
+  for (let i = temperatures.length - 1; i >= 0; i--) {
+    while (stack.length > 0 && temperatures[i] >= temperatures[stack[0]]) {
+      stack.shift();
+    }
+    if (stack.length === 0) {
+      answer[i] = 0;
+    } else answer[i] = stack[0] - i;
+    stack.unshift(i);
+  }
+  return answer;
+};
