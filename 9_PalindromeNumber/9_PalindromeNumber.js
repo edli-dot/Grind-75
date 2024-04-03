1bf017c4be5b499879f3a31b2454dbc63acdd828
Edward Li <edwardli744@gmail.com>
2024-04-04 09:58:11 +1100

    jest test made

--- 9_PalindromeNumber/9_PalindromeNumber.js
+++ 9_PalindromeNumber/9_PalindromeNumber.js
@@ -10,19 +10,21 @@ var isPalindrome = function (x) {
 //This requires the number to be converted to a string
 //can we do this without converting to a string
 
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function (x) {
-  let reverse = 0;
-  let copy = x;
-  while (copy > 0) {
-    reverse = reverse * 10 + (copy % 10);
-    copy = Math.floor(copy / 10);
-  }
-  return reverse === x;
-};
+// /**
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+// var isPalindrome = function (x) {
+//   let reverse = 0;
+//   let copy = x;
+//   while (copy > 0) {
+//     reverse = reverse * 10 + (copy % 10);
+//     copy = Math.floor(copy / 10);
+//   }
+//   return reverse === x;
+// };
 
 //thought process is that we add a digit to reverse while taking away the last digit in copy
 //once all the digits have been moved to reverse aka copy = 0; compare the reverse number to the original
+
+module.exports = isPalindrome;
