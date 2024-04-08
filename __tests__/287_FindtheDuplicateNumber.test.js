3c864fe2c0c322eba1481a10fee5d189d64f7069
Edward Li <edwardli744@gmail.com>
2024-04-08 12:38:31 +1000

    test case for 287

---
+++ __tests__/287_FindtheDuplicateNumber.test.js
@@ -0,0 +1,15 @@
+const findDuplicate = require('../287_FindtheDuplicateNumber/287_FindtheDuplicateNumber.js');
+
+describes('Find the Duplicate Number', () => {
+  it('Finds the duplicate number', () => {
+    const s = [1, 3, 4, 2, 2];
+    const result = findDuplicate(s);
+    expect(result).toEqual(3);
+  });
+
+  it('Finds duplicate number if all nubmers are thee same', () => {
+    const s = [1, 1, 1, 1, 1, 1];
+    const result = findDuplicate(s);
+    expect(result).toEqual(1);
+  });
+});
