cad1e9b9b951a9e9f641dc238d660288bb5e4243
Edward Li <edwardli744@gmail.com>
2024-04-08 11:27:25 +1000

    linked list

---
+++ 237_DeleteNodeinaLinkedList/237_DeleteNodeinaLinkedList.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+  node.val = node.next.val;
+  node.next = node.next.next;
+};
+
+//just skip over the node. can't actually access the previous node since we aren't given the head
