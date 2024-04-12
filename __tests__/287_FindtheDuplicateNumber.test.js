5810f90c91216bbc51dcf47ef17191eb5434d923
Edward Li <edwardli744@gmail.com>
2024-04-12 17:21:56 +1000

    update test

--- __tests__/287_FindtheDuplicateNumber.test.js
+++ __tests__/287_FindtheDuplicateNumber.test.js
@@ -1,10 +1,10 @@
 const findDuplicate = require('../287_FindtheDuplicateNumber/287_FindtheDuplicateNumber.js');
 
-describes('Find the Duplicate Number', () => {
+describe('Find the Duplicate Number', () => {
   it('Finds the duplicate number', () => {
     const s = [1, 3, 4, 2, 2];
     const result = findDuplicate(s);
-    expect(result).toEqual(3);
+    expect(result).toEqual(2);
   });
 
   it('Finds duplicate number if all nubmers are thee same', () => {
