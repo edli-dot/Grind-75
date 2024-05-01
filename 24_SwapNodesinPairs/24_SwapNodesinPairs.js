23c6d9df1b0ff982e2c3f1c23124c7b33a3dc240
Edward Li <edwardli744@gmail.com>
2024-05-02 00:56:34 +1000

    linked list slow fast pointer logic

--- 24_SwapNodesinPairs/24_SwapNodesinPairs.js
+++ 24_SwapNodesinPairs/24_SwapNodesinPairs.js
@@ -3,19 +3,23 @@ var swapPairs = function (head) {
   if (!head || !head.next) return head;
 
   let dummy = new ListNode(0);
+  //this maintains the head that we want to return as the answer
   dummy.next = head;
   let prev = dummy;
 
   while (head && head.next) {
+    //the two pointers that we want to swap
     let first = head;
     let second = head.next;
 
+    //swapping the pointers
     prev.next = second;
     first.next = second.next;
     second.next = first;
-
+    //continues the while loop
     prev = first;
     head = first.next;
   }
+  //returns the top of the modified list which would be the original head
   return dummy.next; // Return the new head after swapping
 };
