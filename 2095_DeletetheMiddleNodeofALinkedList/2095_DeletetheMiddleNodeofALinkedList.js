1eb8a61e2410560fd23a5c1392505381ec00148b
Edward Li <edwardli744@gmail.com>
2024-05-02 01:33:06 +1000

    two pointer linked list problem

---
+++ 2095_DeletetheMiddleNodeofALinkedList/2095_DeletetheMiddleNodeofALinkedList.js
@@ -0,0 +1,19 @@
+var deleteMiddle = function (head) {
+  if (!head || !head.next) return head.next;
+  if (!head.next.next) {
+    head.next = null;
+    return head;
+  }
+  let slow = head;
+  let fast = head.next.next; // Move fast two steps at a time, so slow will be at the middle node when fast reaches the end
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Delete the middle node
+  slow.next = slow.next.next;
+
+  return head;
+};
