e895aee14e26197edea955a8130f2c29900a7446
Edward Li <edwardli744@gmail.com>
2024-05-28 23:29:09 -0700

    test case that requires null. avoids inheritance of prototypes

--- 290_WordPattern/290_WordPattern.js
+++ 290_WordPattern/290_WordPattern.js
@@ -4,22 +4,27 @@
  * @return {boolean}
  */
 var wordPattern = function (pattern, s) {
-  let hash = {};
-  let hash2 = {};
+  let hash = Object.create(null);
+  let hash2 = Object.create(null);
   pattern = pattern.split('');
   s = s.split(' ');
 
   if (pattern.length != s.length) return false;
   for (let i = 0; i < pattern.length; i++) {
-    if (!hash[pattern[i]] && !hash2[s[i]]) {
-      hash[pattern[i]] = s[i];
+    if (hash[pattern[i]] || hash2[s[i]]) {
+      if (hash[pattern[i]] !== s[i] || hash2[s[i]] !== pattern[i]) return false;
     }
-    if (!hash2[s[i]]) {
-      hash2[s[i]] = pattern[i];
-    }
-    console.log(hash, 'hash');
-    console.log(hash2);
-    if (hash[pattern[i]] !== s[i]) return false;
+    if (!hash[pattern[i]]) hash[pattern[i]] = s[i];
+    if (!hash2[s[i]]) hash2[s[i]] = pattern[i];
   }
   return true;
 };
+
+/**
+ * 
+ *pattern="abba" and s="dog constructor constructor dog". results in the test case failing
+ requires the Object.create(null)
+
+ because constructor is a prototype of Object. this is fixed by creating Object.create(null)
+ this creates an object with NO PROTOTYPES
+ */
