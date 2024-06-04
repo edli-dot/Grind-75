3785a7ba3ac8d448f5458c65be0a1bf837167451
Edward Li <edwardli744@gmail.com>
2024-06-04 12:40:49 -0700

    greed

--- 2864_MaxOddBinaryNumber/2864_MaxOddBinaryNumber.js
+++ 2864_MaxOddBinaryNumber/2864_MaxOddBinaryNumber.js
@@ -26,3 +26,11 @@ var maximumOddBinaryNumber = function (s) {
   result += '1';
   return result;
 };
+
+var maximumOddBinaryNumber = function (s) {
+  s = s.split('').sort((a, b) => b - a);
+  s.push(1);
+  s.shift();
+  s = s.join('');
+  return s;
+};
