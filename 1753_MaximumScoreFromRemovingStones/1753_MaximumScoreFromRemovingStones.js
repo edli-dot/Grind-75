00edb24a6ff21c70ce15ae54931f3540e6e8077e
Edward Li <edwardli744@gmail.com>
2024-06-10 14:02:11 -0700

    bitwise right shift operator

--- 1753_MaximumScoreFromRemovingStones/1753_MaximumScoreFromRemovingStones.js
+++ 1753_MaximumScoreFromRemovingStones/1753_MaximumScoreFromRemovingStones.js
@@ -16,3 +16,26 @@ var maximumScore = function (a, b, c) {
   }
   return count;
 };
+
+/**
+ * continue to subtract two from the smallest and largest in the sorted array
+ */
+
+var maximumScore = function (a, b, c) {
+  const piles = [a, b, c].sort((a, b) => a - b);
+  return piles[0] + piles[1] < piles[2]
+    ? piles[0] + piles[1]
+    : (a + b + c) >> 1;
+};
+
+/**
+ * this was a fun one to learn from
+ *
+ * the math behind this is that:
+ * if two smaller piles is less than the largest pile, you can only make moves up to the sum of the smallest piles
+ * if not, then the max moves is the sum of half the total amount of stones
+ *
+ * >> is a bitwise right shift operator
+ * equivalent to dividing the sum by 2
+ * divides by 2 and flors the result
+ */
