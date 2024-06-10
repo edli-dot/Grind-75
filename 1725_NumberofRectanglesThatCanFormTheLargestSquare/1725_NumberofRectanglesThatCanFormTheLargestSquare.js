837462ba9ef32261795ed65fce1f274d67dcfe91
Edward Li <edwardli744@gmail.com>
2024-06-10 13:01:58 -0700

    arrays

--- 1725_NumberofRectanglesThatCanFormTheLargestSquare/1725_NumberofRectanglesThatCanFormTheLargestSquare.js
+++ 1725_NumberofRectanglesThatCanFormTheLargestSquare/1725_NumberofRectanglesThatCanFormTheLargestSquare.js
@@ -11,3 +11,31 @@ var countGoodRectangles = function (rectangles) {
   }
   return count.length;
 };
+
+/**
+ * using in built methods
+ */
+
+var countGoodRectangles = function (rectangles) {
+  let maxLen = 0;
+  let count = 0;
+
+  for (let i = 0; i < rectangles.length; i++) {
+    const l = rectangles[i][0];
+    const w = rectangles[i][1];
+    const side = Math.min(l, w);
+
+    if (side > maxLen) {
+      maxLen = side;
+      count = 1;
+    } else if (side === maxLen) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+/**
+ * works b/c we are loooking for the largest square possible. not the most frequency of the largest square that can be cut from all of the similar size
+ */
