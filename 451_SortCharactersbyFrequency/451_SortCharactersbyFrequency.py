550930eef0655975c93c51a3c44e608258c929a8
Edward Li <edwardli744@gmail.com>
2024-06-20 17:17:53 -0700

    python solution

--- 451_SortCharactersbyFrequency/451_SortCharactersbyFrequency.py
+++ 451_SortCharactersbyFrequency/451_SortCharactersbyFrequency.py
@@ -2,7 +2,9 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         cache = {}
         for letter in s:
+            # python's version of a tertiary operator
             cache[letter] = cache[letter] + 1 if letter in cache else 1
+            # array being sorted, anonymous function being applied, sorts the number in descending order
         vals = sorted(cache.keys(), key=lambda x: cache[x], reverse=True)
         ans = ''.join(char * cache[char] for char in vals)
     
