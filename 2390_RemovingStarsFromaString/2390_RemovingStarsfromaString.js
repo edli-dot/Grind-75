e6ccb6dcc87acda4d1f00bebea3b6f4c1826796e
Edward Li <edwardli744@gmail.com>
2024-07-14 23:49:58 -0700

    array

---
+++ 2390_RemovingStarsFromaString/2390_RemovingStarsfromaString.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+  let ans = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '*') ans.pop();
+    else ans.push(s[i]);
+  }
+  return ans.join('');
+};
