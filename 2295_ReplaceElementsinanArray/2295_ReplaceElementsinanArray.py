542e3c5ba2997be60c15e72cf97c4e70b855c04d
Edward Li <edwardli744@gmail.com>
2024-07-19 15:34:57 -0700

    destruture in for loop

---
+++ 2295_ReplaceElementsinanArray/2295_ReplaceElementsinanArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
+        cache = defaultdict(int)
+        for num in range(len(nums)):
+            cache[nums[num]] = num
+        for x,y in operations:   
+            nums[cache[x]] = y
+            cache[y] = cache[x]
+        return nums
+"""
+another instance of default dictionary int to provide 
+instead of iterating and deconstructing in the loop, you can deconstructure in the actual for loop[]
+"""
